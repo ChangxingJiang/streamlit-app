01137d69b8da94603a2ec1f102f7d2c083d4130c
changxing <1278729001@qq.com>
2024-04-16 08:15:35 +0800

    增加 TODO

--- streamlit_app/__init__.py
+++ streamlit_app/__init__.py
@@ -1,3 +1,8 @@
+"""
+TODO 支持更多命令行参数的情况
+TODO 增加部署时自动创建文件夹工具，增加仅部署不运行的功能
+"""
+
 from streamlit_app.application import Application
 from streamlit_app.errors import StreamlitAppDeployError
 from streamlit_app.page import StreamlitPage
